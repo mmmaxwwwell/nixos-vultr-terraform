e81850cdcdbd9f713581aad8cbbc568b3630a877
mmmaxwwwell <mmmaxwwwell@protonmail.com>
2022-11-04 22:35:32 -0400

    fix path

--- scripts/get-vultr-iso-id.sh
+++ scripts/get-vultr-iso-id.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source settings/secrets.tfvars
+source settings/settings.tfvars
 curl "https://api.vultr.com/v2/iso" \
   -X GET \
   -H "Authorization: Bearer ${vultr_api_key}" | jq
